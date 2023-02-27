efa910073de67dce881b799aa9ddef17e4958274
Jocec31 <revy.cecile@gmail.com>
2023-02-27 09:10:59 +0100

    refactoring code

--- script.js
+++ script.js
@@ -1,40 +1,16 @@
 const button = document.querySelector("button");
-const burger = document.querySelector("button img");
-const menuLeft = document.querySelector(".menu");
-const sections = document.querySelector(".sections");
+const img = document.querySelector("button img");
+const navigation = document.querySelector(".navigation");
 const main = document.querySelector("main");
-const header = document.querySelector("header");
 let toggle = false;
 
 button.addEventListener("click", (e) => {
-  console.log("click");
+  navigation.classList.toggle("active");
+  main.classList.toggle("active");
   toggle = !toggle;
   if (toggle === true) {
-    burger.src = "./ressources/close.svg";
-    menuLeft.style.transform = "translateX(0)";
-    sections.style.transform = "translateX(200px)";
+    img.src = "./ressources/close.svg";
   } else {
-    burger.src = "./ressources/menu.svg";
-    menuLeft.style.transform = "translateX(-100%)";
-    sections.style.transform = "translateX(0)";
+    img.src = "./ressources/menu.svg";
   }
-});
-
-// détecter les resize de window pour faire disparaître le menu latéral ouvert
-// en petit écran et qui persiste si j'aggrandis la fenêtre
-window.addEventListener("resize", (e) => {
-  if (window.innerWidth > 768) {
-    menuLeft.style.transform = "translateX(-100%)";
-    burger.src = "./ressources/menu.svg";
-    menuLeft.style.transform = "translateX(-100%)";
-    sections.style.transform = "translateX(0)";
-    toggle = false;
-  }
-});
-
-// conditionne la width 100% en cas de changement d'orientation
-window.addEventListener("orientationchange", (e) => {
-  //   e.preventDefault();
-  main.style.maxWidth = "100%";
-  header.style.maxWidth = "100%";
 });
