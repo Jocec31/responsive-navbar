95a1432bc88de28a26a5daa8e24d19bbbacfcbd9
Jocec31 <revy.cecile@gmail.com>
2023-02-27 11:41:25 +0100

    mise en place observable js

--- script.js
+++ script.js
@@ -14,3 +14,13 @@ button.addEventListener("click", (e) => {
     img.src = "./ressources/menu.svg";
   }
 });
+
+// surveiller le resize sur ordi pour éviter le bug visuel du transform
+
+new ResizeObserver((entries) => {
+  if (entries[0].contentRect.width <= 1000) {
+    navigation.style.transition = "transform .4s ease-out";
+  } else {
+    navigation.style.transition = "none";
+  }
+}).observe(document.body);
